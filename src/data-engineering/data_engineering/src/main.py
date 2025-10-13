3a92a3a490674d7d119a04fc372c722c17a74bca
Jan Tschada <j.tschada@esri.de>
2025-10-13 19:42:01 +0200

    Info and Describe of a data frame

--- src/data-engineering/data_engineering/src/main.py
+++ src/data-engineering/data_engineering/src/main.py
@@ -4,7 +4,8 @@ from data_engineering.utils import fetch_charging_stations
 def main():
     gis = GIS()
     charging_stations = fetch_charging_stations(gis)
-    print(charging_stations)
+    print(charging_stations.info())
+    print(charging_stations[["Anzahl_Ladepunkte", "Inbetriebnahmedatum"]].describe())
 
 
 if __name__ == "__main__":
