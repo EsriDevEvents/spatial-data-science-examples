03c0521bd7426c50e9f0b211b1698e7eb725db3a
Jan Tschada <j.tschada@esri.de>
2025-10-16 10:26:32 +0200

    Fetching only necessary records

--- src/data-engineering/data_engineering/src/data_engineering/utils.py
+++ src/data-engineering/data_engineering/src/data_engineering/utils.py
@@ -14,7 +14,16 @@ def fetch_charging_stations(gis: GIS, max_record_count: int = 1000) -> GeoAccess
     """
     portal_item: Item = gis.content.get("bc3c97f73d6b4be4921be8560fbc325a")
     feature_layer: FeatureLayer = portal_item.layers[0]
-    feature_sdf = feature_layer.query(where="1=1", out_fields="*", result_record_count=max_record_count, as_df=True)
+    feature_sdf = feature_layer.query(where="1=1", out_fields="*", return_all_records=False, result_record_count=max_record_count, as_df=True)
     return feature_sdf
 
+def fetch_traffic_accidents(gis: GIS, max_record_count: int = 1000) -> GeoAccessor:
+    """Fetches the traffic accidents from the ArcGIS Online feature service.
 
+    Args:
+        gis (GIS): An authenticated GIS object.
+    """
+    portal_item: Item = gis.content.get("027fd014ed184fd78a37b54a68afe892")
+    feature_layer: FeatureLayer = portal_item.layers[0]
+    feature_sdf = feature_layer.query(where="1=1", out_fields="*", return_all_records=False, result_record_count=max_record_count, as_df=True)
+    return feature_sdf
--- src/data-engineering/data_engineering/src/main.py
+++ src/data-engineering/data_engineering/src/main.py
@@ -1,11 +1,15 @@
 from arcgis.gis import GIS
-from data_engineering.utils import fetch_charging_stations
+from data_engineering.utils import fetch_charging_stations, fetch_traffic_accidents
 
 def main():
     gis = GIS()
     charging_stations = fetch_charging_stations(gis)
     print(charging_stations.info())
     print(charging_stations[["Anzahl_Ladepunkte", "Inbetriebnahmedatum"]].describe())
+
+    traffic_accidents = fetch_traffic_accidents(gis)
+    print(traffic_accidents.info())
+    print(traffic_accidents[["UART", "UTYP1"]].describe())
 
 
 if __name__ == "__main__":
